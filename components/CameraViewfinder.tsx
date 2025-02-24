ad599b62eec54a12e609ab8a59f03daab05ab2cd
modamo-gh <morganthemosaic@gmail.com>
2025-02-23 23:19:30 -0500

    Display picture

--- components/CameraViewfinder.tsx
+++ components/CameraViewfinder.tsx
@@ -1,12 +1,13 @@
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
 import { router } from "expo-router";
 import { useEffect, useRef, useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 
 const CameraViewfinder = () => {
 	const [facing, setFacing] = useState<CameraType>("back");
 	const [permissions, setPermissions] = useCameraPermissions();
 	const cameraRef = useRef(null);
+	const [photo, setPhoto] = useState();
 
 	useEffect(() => {
 		if (!permissions?.granted) {
@@ -24,33 +25,40 @@ const CameraViewfinder = () => {
 
 	const takePicture = async () => {
 		if (cameraRef.current) {
-			const photo = await cameraRef.current.takePictureAsync();
-			console.log("Photo taken:", photo.uri);
+			const p = await cameraRef.current.takePictureAsync();
+			setPhoto(p);
 		}
 	};
 
-	return (
-		<CameraView
-			ref={cameraRef}
-			style={{
-				borderRadius: 8,
-				flex: 1,
-				alignItems: "center",
-				justifyContent: "flex-end"
-			}}
-		>
-			<Pressable
-				onPress={async () => takePicture()}
+	{
+		return !photo ? (
+			<CameraView
+				ref={cameraRef}
 				style={{
-					backgroundColor: "#FFFFFF",
-					height: 72,
-					width: 72,
-					borderRadius: 50,
-					marginBottom: 16
+					borderRadius: 8,
+					flex: 1,
+					alignItems: "center",
+					justifyContent: "flex-end"
 				}}
+			>
+				<Pressable
+					onPress={async () => takePicture()}
+					style={{
+						backgroundColor: "#FFFFFF",
+						height: 72,
+						width: 72,
+						borderRadius: 50,
+						marginBottom: 16
+					}}
+				/>
+			</CameraView>
+		) : (
+			<Image
+				source={{ uri: photo.uri }}
+				style={{ height: "100%", width: "100%" }}
 			/>
-		</CameraView>
-	);
+		);
+	}
 };
 
 export default CameraViewfinder;
